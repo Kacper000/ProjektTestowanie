ae31a254661475243c73778d2558bdff62d344e9
NorthDeer <kacperdaniel000@gmail.com>
2019-07-27 14:21:42 +0200

    Sprawdzamy rodzaje trojkatow przez dlugosc bokow

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args) {
+        Trojkat trojkat=new Trojkat(5,5,5);
+
+        System.out.println(trojkat.sprawdz());
+    }
+}
--- src/Trojkat.java
+++ src/Trojkat.java
@@ -1,4 +1,23 @@
-public class Trojkat
-{
+public class Trojkat {
+    int bok1;
+    int bok2;
+    int bok3;
 
+    public Trojkat(int bok1, int bok2, int bok3) {
+        this.bok1 = bok1;
+        this.bok2 = bok2;
+        this.bok3 = bok3;
+    }
+
+    String sprawdz() {
+
+        if (bok1 == bok2 && bok2 == bok3) {
+            return "rownoboczny";
+        } else if (bok1 == bok2 || bok1 == bok3 || bok2 == bok3) {
+            return "rownoramienny";
+        } else {
+            return "roznoboczny";
+        }
+    }
 }
+
